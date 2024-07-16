3936185e5518d7c81ce2ac05cb4558d545e73c84
epenate <epenate@hotmail.com>
2024-07-16 11:10:03 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -11,3 +11,33 @@ function validarIngreso()
         alert("Password no valido !!!! intentelo de Nuevo")
    }
 }
+
+// EESTA ES OTRA FUNCION PARA CUANDO CAMBIE EL SELECT DE LA MARCA
+function precioMarcas()
+{
+    var elementoSeleccionado = document.getElementById("cmbMarca")
+  
+    var mPrecio = 0.0;
+    switch (elementoSeleccionado.value)
+    {
+        case "1":
+              mPrecio =  7000;
+
+              break;
+        case "2":
+                mPrecio =  13000;
+                break;
+        case "3":
+              mPrecio =  6000;
+              break;
+        case "4":
+                mPrecio =  5000;
+                break;
+        case "5":
+                    mPrecio =  7000;
+                    break;
+    }
+
+    document.getElementById("txtPrecio").value = mPrecio;   
+
+}
